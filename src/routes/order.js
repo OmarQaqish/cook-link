c1309bbf8d025f42e781354c2bf7ffbf4c391611
Zehra <95143820+aydinfz@users.noreply.github.com>
2023-07-10 18:32:55 +0300

    feat: create routes of order

---
+++ src/routes/order.js
@@ -0,0 +1,11 @@
+const express = require('express');
+// const orderController = require('../controllers/order');
+const router = express.Router();
+
+/* router.get('/', orderController.getAllOrders);
+router.post('/', orderController.newOrder);
+router.get('/:id', orderController.getOneOrder);
+router.put('/:id', orderController.updateOrder);
+router.delete('/:id', orderController.removeOrder); */
+
+module.exports = router;
