df3614c877993579038e052d83ea5b65ef7a8055
tarik310 <tareq310harh@gmail.com>
2023-07-10 17:34:06 +0300

    add user routes

---
+++ src/routes/user.js
@@ -0,0 +1,19 @@
+const express = require('express');
+// const userController = require('../controllers/user');
+
+const router = express.Router();
+
+// router.get('/profile/:id', userController.getUserProfile);
+// router.put('/profile/:id', userController.updateUserProfile);
+
+// handling user registration
+// router.get('/signin', userController.loginUser);
+// router.post('/signin', userController.loginUser);
+
+// router.get('/signup', userController.createUser);
+// router.post('/signup', userController.createUser);
+
+// router.get('/signout', userController.logoutUser);
+
+// router.get('/authenticated', userController.homePageUser);
+module.exports = router;
