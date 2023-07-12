8d0a034578f1457f364ac996eaf59a8c64ab1ea8
Omar Qaqish <omqaqish@gmail.com>
2023-07-12 14:25:13 +0300

    Feat: add google strategy

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const userRoutes = require('./routes/user');
 const cartRoutes = require('./routes/cart');
 const dishesRoutes = require('./routes/dish');
 const orderRoutes = require('./routes/order');
+const googleAuthRoutes = require('./routes/googleAuth');
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(bodyParser.json());
 // app.use('/api/cart', cartRoutes);
 // app.use('/api/dish', dishesRoutes);
 // app.use('/api/order', orderRoutes);
+app.use('/api/auth/google', googleAuthRoutes);
 
 app.listen(process.env.NODE_LOCAL_PORT, () => {
   console.log(`server listening on port:${process.env.NODE_LOCAL_PORT}`);
