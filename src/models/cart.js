d1bccb1e276ab9d58a8562bfca392a920f5bc3cb
Zehra <95143820+aydinfz@users.noreply.github.com>
2023-07-17 23:19:56 +0300

    update cart schema
    - remove cartItemSchema and simplified main cart schema
    - remove total price

--- src/models/cart.js
+++ src/models/cart.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
-const cartItemSchema = new mongoose.Schema({
+const cartSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
   dish: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Dish',
@@ -12,29 +17,6 @@ const cartItemSchema = new mongoose.Schema({
     min: [1, 'Quantity must be at least 1'],
     max: [10, 'Quantity cannot exceed 10'],
   },
-});
-
-const cartSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  items: [cartItemSchema],
-  totalPrice: {
-    type: Number,
-    required: true,
-  },
-});
-
-// Calculate the total price based on the cart items
-cartSchema.pre('save', function (next) {
-  const totalPrice = this.items.reduce(
-    (sum, item) => sum + item.quantity * item.dish.price,
-    0
-  );
-  this.totalPrice = totalPrice;
-  next();
 });
 
 const Cart = mongoose.model('Cart', cartSchema);
