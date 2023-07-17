4f73ae03d9d172b459651ab24c23e59d1e09afb2
Omar Qaqish <omqaqish@gmail.com>
2023-07-17 20:53:43 +0300

    Feat: add address routes

---
+++ src/routes/address.js
@@ -0,0 +1,10 @@
+const express = require('express');
+// const addressController = require('../controllers/address');
+const router = express.Router();
+
+// router.post('/', addressController.addAddress);
+// router.get('/:id', addressController.getAddress);
+// router.put('/:id', addressController.updateAddress);
+// router.delete('/id', addressController.deleteAddress);
+
+module.exports = router;
