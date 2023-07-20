b64b3ca9ac4d2abc488cf6a3bce031ff00417ecf
Zehra <95143820+aydinfz@users.noreply.github.com>
2023-07-20 16:40:49 +0300

    update order controller

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,8 +1,10 @@
 const express = require('express');
-// const orderController = require('../controllers/order');
+const orderController = require('../controllers/order');
+
 const router = express.Router();
 
-/* router.get('/', orderController.getAllOrders);
+router.get('/', orderController.getAllOrders);
+/*
 router.get('/:id', orderController.getOneOrder);
 router.put('/:id', orderController.updateOrderStatus);
 */
