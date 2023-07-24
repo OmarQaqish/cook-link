3a3d2f2e510b6f38e070cbce0b696814123d69b6
Zehra <95143820+aydinfz@users.noreply.github.com>
2023-07-24 10:23:02 +0300

    create routes and middlewares for order

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const orderController = require('../controllers/order');
+const AuthMiddleware = require('../middlewares/auth');
 
 const router = express.Router();
 
-router.get('/', orderController.getAllOrders);
-/*
-router.get('/:id', orderController.getOneOrder);
-router.put('/:id', orderController.updateOrderStatus);
-*/
+router.get(
+  '/',
+  AuthMiddleware.protectRoute,
+  AuthMiddleware.ristrictTo('admin'),
+  orderController.getAllOrders
+);
+
+router.get(
+  '/assigned-orders',
+  AuthMiddleware.protectRoute,
+  AuthMiddleware.ristrictTo('cook'),
+  orderController.getCookAssignedOrders
+);
+
+router.post(
+  '/payment/:orderId',
+  AuthMiddleware.protectRoute,
+  AuthMiddleware.ristrictTo('user'),
+  orderController.payment
+);
+
+router.put(
+  '/:orderId',
+  AuthMiddleware.protectRoute,
+  AuthMiddleware.ristrictTo('admin', 'cook'),
+  orderController.updateOrderStatus
+);
 
 module.exports = router;
