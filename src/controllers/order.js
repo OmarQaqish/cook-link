f5b5cdb2986afa54c58b53ea51722d4fa1c5da32
Zehra <95143820+aydinfz@users.noreply.github.com>
2023-07-24 12:30:19 +0300

    create getCookAssignedOrders and payment for order

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -9,31 +9,127 @@ const getAllOrders = async (req, res) => {
   }
 };
 
-const getOneOrder = async (req, res) => {
+const getCookAssignedOrders = async (req, res) => {
   try {
-    const order = await Order.findById(req.params.id);
-    if (!order) {
-      return res.status(404).json({ error: 'Dish not found' });
-    }
-    return res.status(200).json(order);
-  } catch (error) {
-    return res.status(500).json({ error: 'Failed to fetch order' });
+    const assignedOrders = await Order.aggregate([
+      {
+        $match: {
+          'items.dish._id': {
+            $in: req.user.dishes,
+          },
+        },
+      },
+      {
+        $addFields: {
+          items: {
+            $filter: {
+              input: '$items',
+              as: 'item',
+              cond: {
+                $in: ['$$item.dish._id', req.user.dishes],
+              },
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          totalPrice: 0,
+        },
+      },
+    ]);
+
+    return res.status(200).json(assignedOrders);
+  } catch (err) {
+    return res.status(500).json({ error: 'Failed to fetch orders' });
   }
 };
 
 const updateOrderStatus = async (req, res) => {
   try {
-    const order = await Order.findByIdAndUpdate(
-      req.params.id,
-      { status: req.body.status },
-      { new: true }
-    );
+    const permittedActions = {
+      admin: ['delivered', 'canceled'],
+      cook: ['pending', 'confirmed', 'prepared'],
+    };
+    const userType = req.user.type;
+    if (!userType) {
+      return res.status(401).json({ error: 'User has no type' });
+    }
 
+    if (!permittedActions[userType]) {
+      return res.status(401).json({ error: 'User is not permitted' });
+    }
+
+    if (!permittedActions[userType].includes(req.body.status)) {
+      return res.status(401).json({
+        error: `You are not permitted to take this action. You are only permitted for these: ${permittedActions[
+          userType
+        ].join(', ')}`,
+      });
+    }
+
+    if (userType === 'admin') {
+      const updatedOrder = await Order.findByIdAndUpdate(
+        req.params.orderId,
+        { status: req.body.status },
+        { new: true }
+      );
+
+      if (!updatedOrder) {
+        return res.status(404).json({ error: 'Order not found' });
+      }
+
+      return res.status(200).json(updatedOrder);
+    }
+
+    if (userType === 'cook') {
+      const order = await Order.findById(req.params.orderId);
+      if (!order) {
+        return res.status(404).json({ error: 'Order not found' });
+      }
+
+      order.items = order.items.map((item) => {
+        if (
+          req.user.dishes
+            .map((d) => d.toString())
+            .includes(item.dish._id.toString())
+        ) {
+          return {
+            ...item.toObject(),
+            status: req.body.status,
+          };
+        }
+        return item.toObject();
+      });
+
+      const updatedOrder = await order.save();
+
+      return res.status(200).json(updatedOrder);
+    }
+
+    return res.status(401).json({ error: 'User type is not existed' });
+  } catch (error) {
+    return res.status(500).json({ error: 'Failed to update order' });
+  }
+};
+
+const payment = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.orderId);
     if (!order) {
       return res.status(404).json({ error: 'Order not found' });
     }
 
-    return res.status(200).json(order);
+    if (order.user.toString() !== req.user.id) {
+      return res
+        .status(401)
+        .json({ message: 'This order is not assigned to you.' });
+    }
+
+    order.status = 'paid';
+    await order.save();
+
+    return res.status(200).json({ message: 'Payment is successful.' });
   } catch (error) {
     return res.status(500).json({ error: 'Failed to update order' });
   }
@@ -41,6 +137,7 @@ const updateOrderStatus = async (req, res) => {
 
 module.exports = {
   getAllOrders,
-  getOneOrder,
   updateOrderStatus,
+  getCookAssignedOrders,
+  payment,
 };
