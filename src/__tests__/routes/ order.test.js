05bd921c1d04a9d65c9ed7f8b935c4b40137cfad
Zehra <95143820+aydinfz@users.noreply.github.com>
2023-07-25 22:44:27 +0300

    create tests

--- src/__tests__/routes/ order.test.js
+++ src/__tests__/routes/ order.test.js
@@ -2,12 +2,11 @@ const request = require('supertest');
 const assert = require('assert');
 const app = require('../../app');
 
-/* request(app)
+request(app)
   .get('/order')
   .expect('Content-Type', /json/)
   .expect('Content-Length', '15')
   .expect(200)
-  .end(function (err, res) {
+  .end((err, res) => {
     if (err) throw err;
   });
- */
