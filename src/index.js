9545fbcce7c166b9c140c47cd1a74ea31d60f97f
Zehra <95143820+aydinfz@users.noreply.github.com>
2023-08-06 15:56:37 +0300

    organize env files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const path = require('path');
 const { app, appListen } = require('./app');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 app.listen(process.env.NODE_LOCAL_PORT, appListen);
